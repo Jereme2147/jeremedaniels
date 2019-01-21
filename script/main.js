02d3e72198926cb3580824bfd26fe497dd2c8a11
Jereme <jereme.daniels@gmail.com>
2019-01-21 17:24:20 -0500

    social links added

--- script/main.js
+++ script/main.js
@@ -3,7 +3,7 @@ let desktop = window.matchMedia("(min-width: 950px)")
 
 $(document).ready(function() {
     if (tablet.matches){
-        $("#photo-div").html(`<img id="photo" src="./media/tablet.png" alt="picture of me">`);
+        $("#photo-div").html(`<img id="photo" src="./media/tablet-w.png" alt="picture of me">`);
     }
     if (desktop.matches){
         $("#photo-div").html(`<img id="photo" src="./media/desktop.png" alt="picture of me">`);
