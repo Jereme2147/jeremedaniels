feb036627e109db29dcda787d6bd48f56dc05fa6
Jereme <jereme.daniels@gmail.com>
2019-01-21 15:32:31 -0500

    first commit

---
+++ script/main.js
@@ -0,0 +1,12 @@
+let tablet = window.matchMedia("(min-width: 600px)");
+let desktop = window.matchMedia("(min-width: 950px)")
+
+$(document).ready(function() {
+    if (tablet.matches){
+        $("#photo-div").html(`<img id="photo" src="./media/tablet.png" alt="picture of me">`);
+    }
+    if (desktop.matches){
+        $("#photo-div").html(`<img id="photo" src="./media/desktop.png" alt="picture of me">`);
+
+    }
+});
