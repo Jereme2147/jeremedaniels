767c70bdeaaaf09e04e37474ec26ddf026a615ab
Jereme <jereme.daniels@gmail.com>
2019-01-22 15:45:57 -0500

    worked contact locations

--- script/main.js
+++ script/main.js
@@ -1,12 +1,15 @@
 let tablet = window.matchMedia("(min-width: 600px)");
-let desktop = window.matchMedia("(min-width: 950px)")
+let desktop = window.matchMedia("(min-width: 950px)");
 
 $(document).ready(function() {
-    if (tablet.matches){
-        $("#photo-div").html(`<img id="photo" src="./media/tablet-w.png" alt="picture of me">`);
+    if (tablet.matches) {
+        $("#photo-div").html(
+            `<img id="photo" src="./media/me.jpg" alt="picture of me">`
+        );
     }
-    if (desktop.matches){
-        $("#photo-div").html(`<img id="photo" src="./media/desktop.png" alt="picture of me">`);
-
+    if (desktop.matches) {
+        $("#photo-div").html(
+            `<img id="photo" src="./media/me.jpg" alt="picture of me">`
+        );
     }
 });
